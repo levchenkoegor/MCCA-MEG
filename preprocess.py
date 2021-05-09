0a31826feec3a1249ef58ad8feb4f979bf983cc7
levchenkoegor <levchenkoegors@gmail.com>
2021-05-09 04:34:50 +0300

    move find_bad_ch_maxwell() to main loop make a func for vis bad channels

--- preprocess.py
+++ preprocess.py
@@ -164,54 +164,45 @@ def ssp_routine(raw, eog_chs, ecg_chs):
 
 
 # Maxwell filtering
-def find_bad_ch_maxwell(raw, visualization=False, savefile=None, crosstalk_file=None, fine_cal_file=None):
-    noisy_chs, flat_chs, auto_scores = mne.preprocessing.find_bad_channels_maxwell(raw,
-                                                                                   cross_talk=crosstalk_file,
-                                                                                   calibration=fine_cal_file,
-                                                                                   min_count=3,
-                                                                                   return_scores=True,
-                                                                                   verbose=False)
-    raw.info['bads'] = raw.info['bads'] + noisy_chs + flat_chs
+def plot_maxwell_bad_ch(auto_scores, savefile=None):
+    ch_type = 'grad'
+    ch_subset = auto_scores['ch_types'] == ch_type
+    ch_names = auto_scores['ch_names'][ch_subset]
+    scores = auto_scores['scores_noisy'][ch_subset]
+    limits = auto_scores['limits_noisy'][ch_subset]
+    bins = auto_scores['bins']  # The the windows that were evaluated.
+    # We will label each segment by its start and stop time, with up to 3
+    # digits before and 3 digits after the decimal place (1 ms precision).
+    bin_labels = [f'{start:3.3f} – {stop:3.3f}' for start, stop in bins]
+    # We store the data in a Pandas DataFrame. The seaborn heatmap function
+    # we will call below will then be able to automatically assign the correct
+    # labels to all axes.
+    data_to_plot = pd.DataFrame(data=scores,
+                                columns=pd.Index(bin_labels, name='Time (s)'),
+                                index=pd.Index(ch_names, name='Channel'))
+    # First, plot the "raw" scores.
+    fig, ax = pyplot.subplots(1, 2, figsize=(12, 8))
+    fig.suptitle(f'Automated noisy channel detection: {ch_type}',
+                 fontsize=16, fontweight='bold')
+    sns.heatmap(data=data_to_plot, cmap='Reds', cbar_kws=dict(label='Score'),
+                ax=ax[0])
+    [ax[0].axvline(x, ls='dashed', lw=0.25, dashes=(25, 15), color='gray')
+     for x in range(1, len(bins))]
+    ax[0].set_title('All Scores', fontweight='bold')
 
-    if visualization:
-        ch_type = 'grad'
-        ch_subset = auto_scores['ch_types'] == ch_type
-        ch_names = auto_scores['ch_names'][ch_subset]
-        scores = auto_scores['scores_noisy'][ch_subset]
-        limits = auto_scores['limits_noisy'][ch_subset]
-        bins = auto_scores['bins']  # The the windows that were evaluated.
-        # We will label each segment by its start and stop time, with up to 3
-        # digits before and 3 digits after the decimal place (1 ms precision).
-        bin_labels = [f'{start:3.3f} – {stop:3.3f}' for start, stop in bins]
-        # We store the data in a Pandas DataFrame. The seaborn heatmap function
-        # we will call below will then be able to automatically assign the correct
-        # labels to all axes.
-        data_to_plot = pd.DataFrame(data=scores,
-                                    columns=pd.Index(bin_labels, name='Time (s)'),
-                                    index=pd.Index(ch_names, name='Channel'))
-        # First, plot the "raw" scores.
-        fig, ax = pyplot.subplots(1, 2, figsize=(12, 8))
-        fig.suptitle(f'Automated noisy channel detection: {ch_type}',
-                     fontsize=16, fontweight='bold')
-        sns.heatmap(data=data_to_plot, cmap='Reds', cbar_kws=dict(label='Score'),
-                    ax=ax[0])
-        [ax[0].axvline(x, ls='dashed', lw=0.25, dashes=(25, 15), color='gray')
-         for x in range(1, len(bins))]
-        ax[0].set_title('All Scores', fontweight='bold')
+    # Now, adjust the color range to highlight segments that exceeded the limit.
+    sns.heatmap(data=data_to_plot,
+                vmin=np.nanmin(limits),  # bads in input data have NaN limits
+                cmap='Reds', cbar_kws=dict(label='Score'), ax=ax[1])
+    [ax[1].axvline(x, ls='dashed', lw=0.25, dashes=(25, 15), color='gray')
+     for x in range(1, len(bins))]
+    ax[1].set_title('Scores > Limit', fontweight='bold')
 
-        # Now, adjust the color range to highlight segments that exceeded the limit.
-        sns.heatmap(data=data_to_plot,
-                    vmin=np.nanmin(limits),  # bads in input data have NaN limits
-                    cmap='Reds', cbar_kws=dict(label='Score'), ax=ax[1])
-        [ax[1].axvline(x, ls='dashed', lw=0.25, dashes=(25, 15), color='gray')
-         for x in range(1, len(bins))]
-        ax[1].set_title('Scores > Limit', fontweight='bold')
+    # The figure title should not overlap with the subplots.
+    fig.tight_layout(rect=[0, 0.03, 1, 0.95])
 
-        # The figure title should not overlap with the subplots.
-        fig.tight_layout(rect=[0, 0.03, 1, 0.95])
-
-        # Save the figure in a subfolder 'Bad_Channels'
-        pyplot.savefig(savefile)
+    # Save the figure in a subfolder 'Bad_Channels'
+    pyplot.savefig(savefile)
 
     return raw
 
